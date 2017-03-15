dcd7e756f975cb523a35514f87ada6b2f3f5d97b
HyunWoong Kim <hyunwoong.kim@vtw.co.kr>
2017-03-15 12:40:10 +0900

    Merged Conflict

--- src/main/java/Test/Demo1/App.java
+++ src/main/java/Test/Demo1/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         // I added comments, too
+    	// added some comments
         System.out.println( "Hello World!" );
         System.out.println( "I'm a king of the world!" );
     }

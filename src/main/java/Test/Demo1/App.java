bdb60d1a28f872de0c1dceaf9686edac5dcc135c
khw1006 <khw1006@gmail.com>
2017-03-15 12:12:24 +0900

    Update App.java

--- src/main/java/Test/Demo1/App.java
+++ src/main/java/Test/Demo1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        // I added comments, too
         System.out.println( "Hello World!" );
         System.out.println( "I'm a king of the world!" );
     }
